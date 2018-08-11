fe535ec527ecb8749ac8dc16f737cc24df42070e
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-08-11 15:06:26 -0400

    minor CSS changes

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -5,7 +5,6 @@ const ArticleList = props => {
   const articles = props.articles
   return articles.map(article => (
     <ul className=".list-unstyled">
-      <h3>All Articles</h3>
       <Article key={article.title} article={article} />
     </ul>
   ))
