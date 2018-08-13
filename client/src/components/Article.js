996157433c44bde507cf95125f54f98b3c40f9a7
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-08-13 15:22:56 -0400

    Adds visual for OutBubble articles

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,17 +1,24 @@
 import React from 'react'
+
 const Article = props => {
   const { article } = props
+
   return (
     article.description.length > 50 && (
       <li className="media border-primary col-lg-11 ">
         <img
           className="mr-3 .img-thumbnail img-small img-fluid"
-          src={
-            article.urlToImage || 'https://bitcoin.org/img/icons/opengraph.png'
-          }
+          src={article.urlToImage}
+          //  || default URL?
           alt=""
         />
         <div className="media-body">
+          {article.out ?
+            <div className="p-3 mb-2 bg-danger text-white">
+              Burst Your Bubble!
+            </div>
+            : null
+          }
           <a href={article.url}>
             <h6>{article.title}</h6>
           </a>
