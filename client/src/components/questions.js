b0193271c243daba70cdd987569f328577fda109
Claire Watson <watson.e.claire@gmail.com>
2018-08-14 18:06:02 -0400

    questions component rendering

--- client/src/components/questions.js
+++ client/src/components/questions.js
@@ -3,30 +3,60 @@ import questionsData from './questionsData'
 import Answers from './answers'
 import Prompt from './prompt'
 import { updateUserThunk } from '../store/user'
+import connect from 'react-redux/lib/connect/connect'
 
 class Questions extends Component {
   constructor() {
     super()
     this.state = {
       questions: [],
-      currentQuestion: 0
+      currentQuestionIndex: 0,
+      score: 0
     }
+    this.handleClick = this.handleClick.bind(TouchList)
   }
 
   async componentDidMount() {
     this.setState({ questions: questionsData })
   }
 
+  async handleClick(evt) {
+    try {
+      //if there are still questions left in array, keep looping through and rendering questions
+      if (this.state.currentQuestionIndex < this.state.questions.length) {
+        this.setState({
+          score: (this.state.score += +evt.target.value),
+          currentQuestionIndex: (currentQuestionIndex += 1)
+        })
+      } else {
+        //when all questions have been aswered, calculate score and dispatch update userThnk
+        let finalScore = Math.round(this.state.score / 2)
+        //TODO: Change to 11 once all Q's are added
+        await this.props.updateUserThunk({ score: finalScore })
+      }
+    } catch (err) {
+      console.err(err.message)
+    }
+  }
+
   render() {
+    const question = this.state.questions[this.state.currentQuestion]
     return (
       <div id={question.id}>
-        <Prompt prompt={question.prompt} />
-        <Answers
-          answers={question.answers}
-          handleClick={this.handleClick}
-          handleSubmit={this.handleSubmit}
-        />
+        <Prompt prompt={question} />
+        <Answers answers={question.answers} handleClick={this.handleClick} />
       </div>
     )
   }
 }
+
+const mapDispatch = dispatch => {
+  return {
+    updateUserThunk: userPrefObj => dispatch(updateUserThunk(userPrefObj))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatch
+)(Quiz)
