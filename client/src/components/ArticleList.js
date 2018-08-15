5a0397a45af9b4035c878f40f886d7a9402ae2a7
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-08-15 15:59:45 -0400

    Converts api/articles PUT route to GET route

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -5,8 +5,8 @@ const ArticleList = props => {
   const articles = props.articles
   if (articles.length) {
     return articles.map(article => (
-      <ul className=".list-unstyled">
-        <Article key={article.title} article={article} />
+      <ul className=".list-unstyled" key={article.url}>
+        <Article article={article} />
       </ul>
     ))
   } else return null
