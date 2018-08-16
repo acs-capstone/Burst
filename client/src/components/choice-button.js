08f00ba4608b7251e728818cec96cdbde27765d4
Claire Watson <watson.e.claire@gmail.com>
2018-08-16 13:03:52 -0400

    saved choice-btn file

--- client/src/components/choice-button.js
+++ client/src/components/choice-button.js
@@ -1,20 +1,8 @@
 import React from 'react'
 
 const ChoiceButton = props => {
-  console.log('*SELECTED TOPICS**', props.selectedTopics)
-  //[{id: 5, name: 'Elections'...}]
-  // console.log('Props.Topic', props.topic)
-  //{id: 5, name: 'Elections'...}
-
-  // const selectedTopicIds = props.selectedTopics.map(topic => {
-  //   console.log('topic.id', topic.id)
-  //   return topic.id
-  // })
-
-  const selected = props.selectedTopics.includes(props.topic.id.toString())
   //returns true or false if the topic is selected on state
-  // const selected = props.selectedTopics.includes(props.topic)
-  console.log('selected', selected)
+  const selected = props.selectedTopics.includes(props.topic.id.toString())
 
   //if array of topics on state contains specific topic, highlight it, otherwise don't
   if (selected) {
