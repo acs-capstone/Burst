f8e553954c93fb77e4e83ed15c7514655dd45d13
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-08-17 19:44:43 -0400

    Adds link to newfeed from Burst in Navbar if logged in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,41 +7,47 @@ import { Quiz } from './Quiz'
 const Navbar = ({ id, handleClick, isLoggedIn }) => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div className="container ">
-      <a className="navbar-brand">Burst</a>
       {isLoggedIn ? (
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            {/* The navbar will show these links after you log in */}
-            <Link className="nav-link" to="/home">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/news">
-              News Feed
-            </Link>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/" onClick={handleClick}>
-              Logout
-            </a>
-          </li>
-        </ul>
-      ) : (
+        <div>
+          {/* <Link className="navbar-brand" to="/news">Burst</Link> */}
           <ul className="navbar-nav ml-auto">
-            {/* The navbar will show these links before you log in */}
+            <Link className="navbar-brand" to="/news">Burst</Link>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
+              {/* The navbar will show these links after you log in */}
+              <Link className="nav-link" to="/home">
+                Home
             </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/signup">
-                Sign Up
+              <Link className="nav-link" to="/news">
+                News Feed
             </Link>
             </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/" onClick={handleClick}>
+                Logout
+            </a>
+            </li>
           </ul>
+        </div>
+      ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <ul className="navbar-nav ml-auto">
+              <a className="navbar-brand">Burst</a>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+            </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">
+                  Sign Up
+            </Link>
+              </li>
+            </ul>
+          </div>
         )}
     </div>
   </nav>
