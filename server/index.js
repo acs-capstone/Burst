aacd1840b4bf8fca3f93e1c63851e60a902a9592
Claire Watson <watson.e.claire@gmail.com>
2018-08-18 19:35:38 -0400

    opentok hooked up to server and client side

--- server/index.js
+++ server/index.js
@@ -79,7 +79,7 @@ const createApp = () => {
   // auth and api routes
   app.use('/auth', require('./auth'))
   app.use('/api', require('./api'))
-  app.use('/videochat', require('./tokbox')
+  app.use('/video', require('./tokbox'))
 
   // static file-serving middleware
   app.use(express.static(path.join(__dirname, '..', 'public')))
--- server/tokbox/index.js
+++ server/tokbox/index.js
@@ -1,23 +1,40 @@
 const OpenTok = require('opentok')
 
-const opentok = new OpenTok(apiKey, apiSecret)
+const router = require('express').Router()
 const apiKey = process.env.API_KEY
 const apiSecret = process.env.API_SECRET
-const router = require('express').Router()
+const opentok = new OpenTok(apiKey, apiSecret)
+const { Session } = require('../db/models')
+
 module.exports = router
 
 router.get('/start', async (req, res, next) => {
-  // Create a session that will attempt to transmit streams directly between
-  // clients. If clients cannot connect, the session uses the OpenTok TURN server:
-
-  const session = opentok.createSession(function(err, session) {
-    if (err) return console.log(err)
-
-    // save the sessionId
-    db.save('session', session.sessionId, done)
+  //see if session is open with one participent (will only have one token)
+  const openSession = await Session.findOne({
+    where: {
+      token2: null
+    }
   })
 
-  const token = sessions.generateToken()
+  //if there is no open session, create a new session and a token for first participant
+  if (openSession === null) {
+    await opentok.createSession(async function(err, session) {
+      if (err) return console.log(err)
+
+      const token1 = await opentok.generateToken(session.sessionId)
+      const newSession = await Session.create({
+        sessionId: session.sessionId,
+        token1
+      })
+
+      res.json({ sessionId: newSession.sessionId, token: token1 })
+    })
+  } else {
+    //otherwise create the second token for second participent to join existing session
+    const token2 = await opentok.generateToken(openSession.sessionId)
+    const fullSession = await openSession.update({ token2 })
+    res.json({ sessionId: fullSession.sessionId, token: token2 })
+  }
 })
 
 router.get('/disconnect', async (req, res, next) => {
