59e9aed8607767bff67e5dac0c7d1011da97d614
Claire Watson <watson.e.claire@gmail.com>
2018-08-21 11:32:27 -0400

    resolved merge conflict

--- server/auth/google.js
+++ server/auth/google.js
@@ -4,32 +4,10 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const { User } = require('../db/models')
 module.exports = router
 
-<<<<<<< HEAD
-const googleClientId = process.env.GOOGLE_CLIENT_ID
-/**
- * For OAuth keys and other secrets, your Node process will search
- * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
- * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - it should NOT be tracked
- * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment variables like so:
- *
- * process.env.GOOGLE_CLIENT_ID = 'your google client id'
- * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
- * process.env.GOOGLE_CALLBACK = '/your/google/callback'
- */
-=======
->>>>>>> master
-
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
 } else {
-<<<<<<< HEAD
-  console.log('in AUTH route')
-=======
   console.log('Found google client ID & client secret!')
->>>>>>> master
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -54,14 +32,10 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
   passport.use(strategy)
 
-<<<<<<< HEAD
-  router.get('/', passport.authenticate('google', { scope: 'email' }))
-=======
   router.get((req, res, next) => {
-    console.log("HIT MEEEEE")
+    console.log('HIT MEEEEE')
     passport.authenticate('google', { scope: 'email' })
   })
->>>>>>> master
 
   router.get(
     '/callback',
