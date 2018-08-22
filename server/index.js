116b17cb5feb68b39de64ba443ad6e3b2e7eadea
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-08-22 10:24:18 -0400

    Saving

--- server/index.js
+++ server/index.js
@@ -88,7 +88,7 @@ const createApp = () => {
 
   // auth and api routes
   app.use('/auth', require('./auth'))
-  app.use('/api', require('./api'))
+  app.use('api', require('./api'))
   app.use('/video', require('./video'))
 
   // static file-serving middleware
